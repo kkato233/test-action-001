f92d9ca569b2faf4503cc845bbb57d9c1341a53d
Kazuhiro Kato <kazuhiro.kato@hotmail.co.jp>
2021-01-31 16:52:43 +0900

    最初の登録

---
+++ MySite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
